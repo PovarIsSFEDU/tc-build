896c14202955be241bb962beeb3bf7e99159a7d5
PovarIsSFEDU <plukash@sfedu.ru>
2023-12-29 18:12:49 +0300

    Removed error, added tests

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import sys
+
 
 def main(fst, sec):
-    return fst/sec
+    return fst + sec
+
+
+def run_tests():
+    assert main(0, 0) == 0
+    assert main(1, 2) == 3
+    assert main(2, -1) != -4
 
 
 if __name__ == "__main__":
+    args = sys.argv
+    if len(args) > 1:
+        run_tests()
     main(1, 0)
